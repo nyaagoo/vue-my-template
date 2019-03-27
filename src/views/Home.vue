e8488c48ed5923032e48130086ab88ada0dc31dc
nyagoo <acc.mg47@gmail.com>
2019-03-27 22:12:22 +0900

    [Update] install vuex-module-decorators

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   .home
     img(alt="Vue logo" src="../assets/logo.png")
-    HelloWorld(msg="Welcome to Your Vue.js + TypeScript App")
+    HelloWorld(msg="Welcome to Your Vue.js + TypeScript App2")
 </template>
 
 <script lang="ts">
